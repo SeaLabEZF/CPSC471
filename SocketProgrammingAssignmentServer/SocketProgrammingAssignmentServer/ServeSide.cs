91b18316078c6dca043b1014c91954ac7c35081d
SeaLabEZF <sean.wulwick@gmail.com>
2017-11-13 16:34:30 -0800

    Rework
    redesigned the server side stuff, it's not complete but its getting
    there.

--- SocketProgrammingAssignmentServer/SocketProgrammingAssignmentServer/ServeSide.cs
+++ SocketProgrammingAssignmentServer/SocketProgrammingAssignmentServer/ServeSide.cs
@@ -6,143 +6,269 @@ using System.Net;
 using System.Text;
 using System.Net.Sockets;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerSide
 {
     public class FtpServer
     {
-        //create a listener for TCP events
-        private TcpListener listener;
+        private TcpListener _listener;
 
-        //initializer for the FTP Server
         public FtpServer()
         { }
 
-        //starts the TCP listener and sends the connections to HandleAcceptTcpClient
         public void Start(uint port_number)
         {
-            listener = new TcpListener(IPAddress.Any, (int)port_number);
-            listener.Start();
-            listener.BeginAcceptTcpClient(HandleAcceptTcpClient, listener);
+            _listener = new TcpListener(IPAddress.Any, (int)port_number);
+            _listener.Start();
+            _listener.BeginAcceptTcpClient(HandleAcceptTcpClient, _listener);
         }
 
-        //stops the listener from listening to TCP events
         public void Stop()
         {
-            if (listener != null)
+            if (_listener != null)
             {
-                listener.Stop();
+                _listener.Stop();
             }
         }
 
-        //called whenever a client connects to the server
-        private async void HandleAcceptTcpClient(IAsyncResult result)
+        private void HandleAcceptTcpClient(IAsyncResult result)
         {
-            //get a refernce to the client
-            TcpClient client = listener.EndAcceptTcpClient(result);
 
-            //further connectsion will also be handled by this method
-            listener.BeginAcceptTcpClient(HandleAcceptTcpClient, listener);
+            _listener.BeginAcceptTcpClient(HandleAcceptTcpClient, _listener);
+            TcpClient client = _listener.EndAcceptTcpClient(result);
 
-            //get the stream from the client and set up a reader and a writer
-            NetworkStream stream = client.GetStream();
-            using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII))
-            using (StreamReader reader = new StreamReader(stream, Encoding.ASCII))
+            ClientConnection connection = new ClientConnection(client);
+
+            ThreadPool.QueueUserWorkItem(connection.HandleClient, client);
+        }
+    }
+
+    public class ClientConnection
+    {
+        //create a listener for TCP events
+        private TcpClient _controlClient;
+        private TcpClient _dataClient;
+
+
+        private NetworkStream _controlStream;
+        private StreamReader _controlReader;
+        private StreamWriter _controlWriter;
+        private string _CurrentDirectory = "\\";
+        private string _transferType;
+        private IPEndPoint _dataEndpoint;
+        private TcpListener _passiveListener;
+        private Array portArray;
+        private StreamReader _dataReader;
+        private StreamWriter _dataWriter;
+
+        public ClientConnection(TcpClient client)
+        {
+            _controlClient = client;
+
+            _controlStream = _controlClient.GetStream();
+
+            _controlReader = new StreamReader(_controlStream);
+            _controlWriter = new StreamWriter(_controlStream);
+            
+
+        }
+
+        public void HandleClient(object obj)
+        {
+            _controlWriter.WriteLine("220 Service Ready.");
+            _controlWriter.Flush();
+
+            string line;
+
+            try
             {
-                //send this to let the client know we accepted the connection
-                writer.Write("ftp>");
-                writer.Flush();
-
-                //we will keep looping later until we quit
-                bool quit = false;
-
-                //read in the inital line
-                string line = await reader.ReadLineAsync();
-
-                //while (!quit && !string.IsNullOrEmpty(line))
-                //loop until we are told to quit
-                while (!quit)
+                while (!string.IsNullOrEmpty(line = _controlReader.ReadLine()))
                 {
-                    //split the line by spaces
-                    var args = line.Split(' ');
-                    if (args.Length > 0)
+                    string response = null;
+
+                    string[] command = line.Split(' ');
+
+                    string cmd = command[0].ToUpperInvariant();
+                    string arguments = command.Length > 1 ? line.Substring(command[0].Length + 1) : null;
+                    if (string.IsNullOrWhiteSpace(arguments))
+                        arguments = null;
+
+                    if (response == null)
                     {
-                        if (args[0] == "get")
+                        switch (cmd)
                         {
-                            //set up a new connection to send the file
-                            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("something");
-                            request.Method = WebRequestMethods.Ftp.UploadFile;
+                            case "TYPE":
+                                string[] splitArgs = arguments.Split(' ');
+                                response = Type(splitArgs[0], splitArgs.Length > 1 ? splitArgs[1] : null);
+                                break;
+                            case "PASV":
+                                response = Passive();
+                                break;
+                            case "GET":
+                                //response = GET(arguments);
+                                break;
+                            case "PUT":
+                                break;
+                            case "LS":
+                                //response = LS(arguments);
+                                break;
+                            case "QUIT":
+                                response = "221 Service closing control connection";
+                                break;
 
-                            StreamReader sourceStream = new StreamReader(args[1]);
-                            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
-                            sourceStream.Close();
-                            request.ContentLength = fileContents.Length;
-
-                            Stream requestStream = request.GetRequestStream();
-                            requestStream.Write(fileContents, 0, fileContents.Length);
-                            requestStream.Close();
-
-                            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-
-                            writer.WriteLine("Download Complete, status {0}", response.StatusCode);
-
-                            response.Close();
+                            default:
+                                response = "502 Command not implemented";
+                                break;
                         }
-                        else if (args[0] == "put")
-                        {
-                            //set up a new connection to recieve the file
-                            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("something");
-                            request.Method = WebRequestMethods.Ftp.DownloadFile;
+                    }
 
-                            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-
-                            Stream responseStream = response.GetResponseStream();
-                            StreamReader dataReader = new StreamReader(responseStream);
-                            writer.WriteLine(dataReader.ReadToEnd());
-
-                            writer.WriteLine("Upload Complete, status {0}", response.StatusCode);
-
-                            dataReader.Close();
-                            response.Close();
-                        }
-                        else if (args[0] == "ls") //possibly add 'cd' - or are we supposed to make a 'ls -l' like directory tree?
-                        {
-                            string temp = null;
-                            //list the file
-                            Process proc = new Process();
-                            
-                            proc.StartInfo.FileName = "cmd.exe";
-                            temp = "dir";
-                            proc.StartInfo.CreateNoWindow = true;
-                            proc.StartInfo.RedirectStandardInput = true;
-                            proc.StartInfo.RedirectStandardOutput = true;
-                            proc.StartInfo.UseShellExecute = false;
-                            proc.Start();
-                            proc.StandardInput.WriteLine(temp);
-                            proc.StandardInput.Flush();
-                            proc.StandardInput.Close();
-                            proc.WaitForExit();
-                            writer.Write("ftp>" + proc.StandardOutput.ReadToEnd());
-                        }
-                        else if (args[0] == "quit")
-                        {
-                            //set quit to true
-                            quit = true;
-                        }
-                        else
-                        {
-                            writer.WriteLine("'" + line + "' isn't an accepted command.");
-                        }
+                    if (_controlClient == null || !_controlClient.Connected)
+                    {
+                        break;
                     }
                     else
                     {
-                        writer.WriteLine("<Empty Line> isn't an accepted command.");
+                        _controlWriter.WriteLine(response);
+                        _controlWriter.Flush();
+
+                        if (response.StartsWith("221"))
+                        {
+                            break;
+                        }
                     }
-                    writer.Flush();
-                    line = await reader.ReadLineAsync();
+
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private string Type(string typeCode, string formatControl)
+        {
+            string response = "500 ERROR";
+
+            switch (typeCode)
+            {
+                case "A":
+                    response = "200 OK";
+                    break;
+                case "I":
+                    _transferType = typeCode;
+                    response = "200 OK";
+                    break;
+                case "E":
+                case "L":
+                default:
+                    response = "504 Command not implemented for that parameter.";
+                    break;        
+            }
+
+            if(formatControl != null)
+            {
+                switch (formatControl)
+                {
+                    case "N":
+                        response = "200 OK";
+                        break;
+                    case "T":
+                    case "C":
+                    default:
+                        response = "504 Command not implemented for that parameter.";
+                        break;
+                }
+            }
+
+            return response;
+        }
+
+        private string Passive()
+        {
+            IPAddress localAddress = ((IPEndPoint)_controlClient.Client.LocalEndPoint).Address;
+
+            _passiveListener = new TcpListener(localAddress, 0);
+            _passiveListener.Start();
+
+            IPEndPoint localEndpoint = ((IPEndPoint)_passiveListener.LocalEndpoint);
+
+            byte[] address = localEndpoint.Address.GetAddressBytes();
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(portArray);
+
+            return string.Format("227 Entering Passive Mode.");
+        }
+
+        private string List(string pathname)
+        {
+            if (pathname == null)
+                pathname = string.Empty;
+
+            pathname = new DirectoryInfo(Path.Combine(_CurrentDirectory, pathname)).FullName;
+
+            if (Directory.Exists(pathname))
+            {
+                _dataClient = new TcpClient();
+                _dataClient.BeginConnect(_dataEndpoint.Address, _dataEndpoint.Port, DoList, pathname);
+
+                return string.Format("150 LS");
+            }
+            return "450 Requested file action not taken";
+        }
+
+        private void DoList(IAsyncResult result)
+        {
+            _dataClient.EndConnect(result);
+
+            string pathname = (string)result.AsyncState;
+
+            using (NetworkStream dataStream = _dataClient.GetStream())
+            {
+                _dataReader = new StreamReader(dataStream, Encoding.ASCII);
+                _dataWriter = new StreamWriter(dataStream, Encoding.ASCII);
+            }
+
+            IEnumerable<string> directories = Directory.EnumerateDirectories(pathname);
+
+            foreach (string dir in directories)
+            {
+                DirectoryInfo d = new DirectoryInfo(dir);
+
+                string date = d.LastWriteTime < DateTime.Now - TimeSpan.FromDays(180) ?
+                    d.LastWriteTime.ToString("MMM dd yyyy") :
+                    d.LastWriteTime.ToString("MMM dd HH:mm");
+
+                string line = string.Format("drwxr-x-x  2   2003    2003    {0,8} {1} {2}", "4096", date, d.Name);
+
+                _dataWriter.WriteLine(line);
+                _dataWriter.Flush();
+            }
+
+            IEnumerable<string> files = Directory.EnumerateFiles(pathname);
+
+            foreach (string file in files)
+            {
+                FileInfo f = new FileInfo(file);
+
+                string date = f.LastWriteTime < DateTime.Now - TimeSpan.FromDays(180) ?
+                    f.LastWriteTime.ToString("MMM dd yyyy") :
+                    f.LastWriteTime.ToString("MMM dd HH:mm");
+
+                string line = string.Format("-rw-r--r--     2 2003  2003    {0,8} {1} {2}", f.Length, date, f.Name);
+
+                _dataWriter.WriteLine(line);
+                _dataWriter.Flush();
+            }
+
+            _dataClient.Close();
+            _dataClient = null;
+
+            _controlWriter.WriteLine("226 Transfer complete");
+            _controlWriter.Flush();
         }
     }
 }
